6c8f7991b92fbc04cbd6e36555364fbe889dff72
William Crowell <william.crowell@openlogic.com>
2016-05-12 16:12:45 -0400

    Added ticket validation step to ValidateSpec

--- src/test/groovy/ValidateSpec.groovy
+++ src/test/groovy/ValidateSpec.groovy
@@ -1,5 +1,9 @@
 import pages.LoginPage
 import groovyx.net.http.RESTClient
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.HttpURLClient
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
 
 class ValidateSpec extends CommonGebSpec {
 	def setup() {
@@ -13,15 +17,28 @@ class ValidateSpec extends CommonGebSpec {
 		def client = new RESTClient(browser.config.baseUrl)
 		// If using SSL, then this method must be called or else a javax.net.ssl.SSLHandshakeException will result due to a self-signed certificate in /etc/jetty/keystore.
 		client.ignoreSSLIssues()
-		
-		def resp = client.post( path : "/" + properties."cas.context.root" + "/v1/tickets",
+
+		// Get a ticket granting ticket		
+		def respTgt = client.post( path : "/" + properties."cas.context.root" + "/v1/tickets",
 			body : [ username: properties.username, password: properties.password ],
 			requestContentType : "application/x-www-form-urlencoded" )
-		
-		assert resp.status == 201
-		def url = resp.headers.Location
+
+		assert respTgt.status == 201
+		def url = respTgt.headers.Location
 		def String[] nodes = url.split("/")
 		def ticket = nodes.getAt(nodes.length - 1)
 		println "ticket: $ticket"
+		
+		// Get a service ticket	using the ticket granting ticket
+		def respSt = client.post( path : "/" + properties."cas.context.root" + "/v1/tickets/$ticket",
+			body : [ service: "$baseUrl/protected-web-app/" ],
+			requestContentType : "application/x-www-form-urlencoded" )
+
+		assert respSt.status == 200
+		def serviceTicket = respSt.getData()
+
+		// Validate the service ticket
+		go "/" + properties."cas.context.root" + "/validate?service=$baseUrl/protected-web-app/&ticket=$serviceTicket"
+		assert $("pre").text() == "yes\ncasuser"
     }
 }
